9ccfee38e7c36bda13513b98c6652834f8b587da
Arnav Verma <arnav.verma@mail.utoronto.ca>
2022-03-13 16:45:44 -0400

    added styles to tailwind

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Main() {
   return (
-	<div>Main</div>
+	<div className="w-32 h-32 bg-blue-100"><canvas className="w-32 h-64 bg-green-400" /></div>
   )
 }
 
--- src/renderer.ts
+++ src/renderer.ts
@@ -26,7 +26,8 @@
  * ```
  */
 
-import './index.css';
+// import './index.css';
+import './index.css'
 import './app';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
